12fd584ca1668293d392776d51b2c3a1f61aba5a
AWEntenman <awe@cosecom.com>
2016-12-12 15:31:31 -0500

    first commt, program is  incomplete

--- Pentagonal_Numbers/Pentagonal_Numbers.cpp
+++ Pentagonal_Numbers/Pentagonal_Numbers.cpp
@@ -4,36 +4,46 @@
 #include "stdafx.h"
 using namespace std;
 
-const int tblSz = 100000;
+const int tblSz = 10000;
 
 int main()
 {
-	long long pentagonalNums[tblSz];
-	int i, j;
-	long long sm, df;//sum and difference
+	long double pentagonalNums[tblSz];
+	int i, j, k, n;
+	long double sm, df;//sum and difference
+	long double sms, dfs;
 	char x;
+
+
 	//
 	// create table of first tblSz pentagonal numbers
 	//
-	for ( i = 1; i <= tblSz; i++)
+	for (i = 1; i <= tblSz; i++)
 	{
-		pentagonalNums[i-1] = i*(3 * i - 1) / 2;
+		pentagonalNums[i - 1] = i*(3 * i - 1) / 2;
 	}
 	//
 	//Search for matching sum and differinces of pentagonal numbers.
 	//
-	for ( i = 0; i < tblSz-1; i++)
+	for (i = 0; i < tblSz - 1; i++)
 	{
-		for ( j = i+1; j < tblSz; j++)
+		for (j = i + 1; j < tblSz; j++)
 		{
 			sm = pentagonalNums[j] + pentagonalNums[i];
-			df = pentagonalNums[j] - pentagonalNums[i];
-			
+			sms = (sqrt(24 * sm + 1) + 1) / 6;
+			if (sms == floor(sms))
+			{
+				df = pentagonalNums[j] - pentagonalNums[i];
+				dfs = (sqrt(24 * df + 1) + 1) / 6;
+				if (dfs == floor(dfs))
+				{
+					cout << "found " << sm << " and " << df << endl;
+				}
+			}
 		}
+		cout << "Finished, type q to close" << endl;
+		cin >> x;
+
+		return 0;
 	}
-	cout << "Finished, type q to close" << endl;
-	cin >> x;
-
-    return 0;
 }
-
